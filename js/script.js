2aa9763d1f9a2492a8db579046d28b5929d12d42
giordanoale <a.giordano89@gmail.com>
2021-03-18 16:27:13 +0100

    Aggiunto ciclo che aggiunge gli elementi dell'array sulla pagina

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+//Definisco un array di elementi
+var listaSpesa = [
+    "Riviste ose",
+    "Supposte di glicerina",
+    "Bottiglia di whisky",
+    "Un paio di confezioni salvaslip",
+    "Fuochi d'artificio illegali",
+    "Clistere usa e getta",
+]
+
+console.log(listaSpesa);
+
 //Con questa funzione si elimina l'elemento
 $(".to-do").on("click", ".delete", function(){
     $(this).parent().remove();
 });
+
+// Questo ciclo aggiunge gli elementi dell'array all'interno della lista 
+for (var i = 0; i < listaSpesa.length; i++) {
+    var template = $(".template li").clone();
+
+    template.prepend(listaSpesa[i]);
+
+    $(".to-do").append(template);
+}
